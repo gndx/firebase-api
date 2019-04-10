1f0b27465ba56b4bede8c09aa593aabb718b7fbf
Oscar Barajas <oscar@dragons.mx>
2019-04-09 23:05:03 -0500

    Create a Functions API

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
+// Requerimos Firebase Admin
+const firebase = require('firebase-admin');
+// Creamos un archivo con nuestra configuración.
+const config = require('./firebase-config.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// inicializamos nuestra aplicacion
+firebase.initializeApp({
+  credential: firebase.credential.cert(config),
+  databaseURL: 'https://arepa-dev-api.firebaseio.com'
+});
+
+// creaamos la funcion que obtiene los recursos de nuestra firebase database
+exports.api = functions.https.onRequest((req, res) => {
+  res.header('Content-Type', 'application/json');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'GET') {
+    const data = firebase.database().ref('/me')
+    data.on('value', (snapshot) => {
+      res.json(snapshot.val());
+    });
+  }
+});
